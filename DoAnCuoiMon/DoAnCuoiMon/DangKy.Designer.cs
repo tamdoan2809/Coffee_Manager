1c99eba998834efd843420a0b6d6f8f6cc133836
tamdoan2809 <kenboy2xdt@gmail.com>
2024-10-06 19:32:50 +0700

    interfaceDangKy

---
+++ DoAnCuoiMon/DoAnCuoiMon/DangKy.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace DoAnCuoiMon
+{
+    partial class DangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_tk = new System.Windows.Forms.TextBox();
+            this.btn_dangKy = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_mk = new System.Windows.Forms.TextBox();
+            this.btn_dn = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(322, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng Ký";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txt_tk
+            // 
+            this.txt_tk.Location = new System.Drawing.Point(228, 100);
+            this.txt_tk.Name = "txt_tk";
+            this.txt_tk.Size = new System.Drawing.Size(422, 27);
+            this.txt_tk.TabIndex = 1;
+            // 
+            // btn_dangKy
+            // 
+            this.btn_dangKy.Location = new System.Drawing.Point(168, 252);
+            this.btn_dangKy.Name = "btn_dangKy";
+            this.btn_dangKy.Size = new System.Drawing.Size(94, 29);
+            this.btn_dangKy.TabIndex = 2;
+            this.btn_dangKy.Text = "Đăng ký";
+            this.btn_dangKy.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(122, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tài khoản";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(122, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mật khẩu";
+            // 
+            // txt_mk
+            // 
+            this.txt_mk.Location = new System.Drawing.Point(228, 164);
+            this.txt_mk.Name = "txt_mk";
+            this.txt_mk.Size = new System.Drawing.Size(422, 27);
+            this.txt_mk.TabIndex = 5;
+            // 
+            // btn_dn
+            // 
+            this.btn_dn.Location = new System.Drawing.Point(370, 252);
+            this.btn_dn.Name = "btn_dn";
+            this.btn_dn.Size = new System.Drawing.Size(94, 29);
+            this.btn_dn.TabIndex = 6;
+            this.btn_dn.Text = "Đăng nhập";
+            this.btn_dn.UseVisualStyleBackColor = true;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(556, 252);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(94, 29);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "Thoát";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // DangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 315);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_dn);
+            this.Controls.Add(this.txt_mk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_dangKy);
+            this.Controls.Add(this.txt_tk);
+            this.Controls.Add(this.label1);
+            this.Name = "DangKy";
+            this.Text = "DangKy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txt_tk;
+        private Button btn_dangKy;
+        private Label label2;
+        private Label label3;
+        private TextBox txt_mk;
+        private Button btn_dn;
+        private Button btn_exit;
+    }
+}
